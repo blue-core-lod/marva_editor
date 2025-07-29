962a39a220c61f5d04c2cdd0a6774a17be68e6ed
Jeremy Duncan <jpd294@cornell.edu>
2025-07-29 14:29:10 -0400

    added login path

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,16 @@ import HomeView from "../views/HomeView.vue";
 import Edit from "../views/Edit.vue";
 import Load from "../views/Load.vue";
 import EditMulti from "../views/EditMulti.vue";
+import Login from "../views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+    },
     {
       path: "/",
       name: "home",
